f282cdc6202c89a82eb727ede3fb44d989f9b72e
Snowflake Demo <demo@snowflake.com>
2026-02-05 20:46:43 -0700

    Fix CREATE NOTEBOOK PROJECT - add FROM clause with workspace path

--- 02_create_notebook_project.sql
+++ 02_create_notebook_project.sql
@@ -1,10 +1,11 @@
 -- Create Notebook Project
--- Run after setting up the notebook from Git
+-- Update <YOUR_WORKSPACE_HASH> with your actual workspace hash
 
 USE DATABASE DEMO_DB;
 USE SCHEMA PUBLIC;
 
 CREATE OR REPLACE NOTEBOOK PROJECT CARBON_EMISSIONS_PROJECT
+  FROM 'snow://workspace/<YOUR_WORKSPACE_HASH>/carbon_emissions_calculator.ipynb'
   COMMENT = 'Parameterized emissions calculator';
 
 SHOW NOTEBOOK PROJECTS LIKE 'CARBON_EMISSIONS_PROJECT';
